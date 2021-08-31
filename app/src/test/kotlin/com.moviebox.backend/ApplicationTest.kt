bd19764f2e79e90f455357a9f4388544a2cb9e22
Rodion <belovickiy.rodion@gmail.com>
2021-08-31 23:18:49 +0300

    Fix tests

--- app/src/test/kotlin/com.moviebox.backend/ApplicationTest.kt
+++ app/src/test/kotlin/com.moviebox.backend/ApplicationTest.kt
@@ -4,8 +4,8 @@ import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.withTestApplication
 import io.ktor.server.testing.handleRequest
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class ApplicationTest {
     @Test
